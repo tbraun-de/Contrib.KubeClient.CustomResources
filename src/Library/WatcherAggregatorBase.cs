9c80c98f558f88ae65a99a8f20c28edbbdc329b4
Bastian Eicher <bastian.eicher@axoom.com>
2018-09-25 14:27:41 +0200

    Adds WatcherAggregatorBase

---
+++ src/Library/WatcherAggregatorBase.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
+
+namespace Contrib.KubeClient.CustomResources
+{
+    /// <summary>
+    /// Base class for building classes that aggregate and debounce events from one more <see cref="ICustomResourceWatcher"/>s.
+    /// </summary>
+    [PublicAPI]
+    public abstract class WatcherAggregatorBase : IDisposable
+    {
+        private readonly IScheduler _scheduler;
+        private IDisposable _subscription;
+
+        protected WatcherAggregatorBase(ILogger<WatcherAggregatorBase> logger,
+                                        TimeSpan debounceDuration,
+                                        IEnumerable<ICustomResourceWatcher> watchers,
+                                        IScheduler scheduler = null)
+        {
+            Logger = logger;
+            DebounceDuration = debounceDuration;
+            Watchers = watchers;
+            _scheduler = scheduler ?? Scheduler.Default;
+
+            Subscribe();
+        }
+
+        protected ILogger Logger { get; }
+        protected TimeSpan DebounceDuration { get; }
+        protected IEnumerable<ICustomResourceWatcher> Watchers { get; }
+
+        protected void Subscribe()
+        {
+            _subscription?.Dispose();
+
+            var observables = Watchers.Select(watcher => Observable.FromEventPattern(addHandler => watcher.DataChanged += addHandler,
+                                                                                     removeHandler => watcher.DataChanged -= removeHandler));
+
+            _subscription = observables
+                           .Merge()
+                           .Throttle(DebounceDuration, _scheduler)
+                           .Subscribe(OnNext);
+        }
+
+        protected virtual void OnNext(EventPattern<object> obj)
+        {
+            try
+            {
+                OnChanged();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogWarning(ex, $"Execution of {nameof(OnChanged)} failed");
+            }
+        }
+
+        protected abstract void OnChanged();
+
+        public void Dispose() => _subscription?.Dispose();
+    }
+}
---
+++ src/UnitTests/WatcherAggregatorFacts.cs
@@ -0,0 +1,59 @@
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Reactive.Testing;
+using Moq;
+using Moq.Protected;
+using Xunit;
+
+namespace Contrib.KubeClient.CustomResources
+{
+    public class WatcherAggregatorFacts : IDisposable
+    {
+        private readonly Mock<ICustomResourceWatcher<CustomResource<string>>> _watcherMock = new Mock<ICustomResourceWatcher<CustomResource<string>>>();
+        private readonly Mock<ICustomResourceWatcher<CustomResource<string>>> _watcherMock2 = new Mock<ICustomResourceWatcher<CustomResource<string>>>();
+        private readonly TestScheduler _testScheduler = new TestScheduler();
+        private readonly TimeSpan _debounceDuration = TimeSpan.FromMinutes(1);
+        private readonly Mock<WatcherAggregatorBase> _aggregatorMock;
+        private readonly WatcherAggregatorBase _aggregator;
+
+        public WatcherAggregatorFacts()
+        {
+            _aggregatorMock = new Mock<WatcherAggregatorBase>(
+                MockBehavior.Default,
+                new Logger<WatcherAggregatorBase>(new LoggerFactory()),
+                _debounceDuration,
+                new[] {_watcherMock.Object, _watcherMock2.Object},
+                _testScheduler)
+            {
+                CallBase = true
+            };
+            _aggregator = _aggregatorMock.Object;
+        }
+
+        public void Dispose() => _aggregator.Dispose();
+
+        [Fact]
+        public void DebouncesWatcherEvents()
+        {
+            _watcherMock.Raise(watcher => watcher.DataChanged += null, 0, EventArgs.Empty);
+            _watcherMock2.Raise(watcher => watcher.DataChanged += null, 1, EventArgs.Empty);
+
+            _aggregatorMock.Protected().Verify("OnChanged", Times.Never());
+            _testScheduler.AdvanceBy(_debounceDuration.Ticks);
+            _aggregatorMock.Protected().Verify("OnChanged", Times.Once());
+        }
+
+        [Fact]
+        public void FailingOnChangedDoesNotStopStream()
+        {
+            _aggregatorMock.Protected().Setup("OnChanged").Throws<Exception>();
+
+            _watcherMock.Raise(watcher => watcher.DataChanged += null, 0, EventArgs.Empty);
+            _testScheduler.AdvanceBy(_debounceDuration.Ticks);
+            _watcherMock.Raise(watcher => watcher.DataChanged += null, 0, EventArgs.Empty);
+            _testScheduler.AdvanceBy(_debounceDuration.Ticks);
+
+            _aggregatorMock.Protected().Verify("OnChanged", Times.Exactly(2));
+        }
+    }
+}
