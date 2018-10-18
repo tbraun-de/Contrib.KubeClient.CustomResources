5b6a8517ea40123d3ec015a60138e83ef4270a10
Matthias Rombach <matthias.rombach@axoom.com>
2018-10-18 09:47:47 +0200

    Updates KubeClient

--- src/Library/KubeApiClientFactory.cs
+++ src/Library/KubeApiClientFactory.cs
@@ -2,8 +2,6 @@ using System;
 using System.IO;
 using System.Runtime.InteropServices;
 using KubeClient;
-using KubeClient.Extensions.AuthProviders.Gcp;
-using KubeClient.MessageHandlers;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -22,7 +20,6 @@ namespace Contrib.KubeClient.CustomResources
 
         public IKubeApiClient Build()
         {
-            IAuthProviderConfigurationStrategy authProviderStrategy = null;
             KubeClientOptions options;
             if (!string.IsNullOrWhiteSpace(_connectionString))
             {
@@ -32,8 +29,6 @@ namespace Contrib.KubeClient.CustomResources
             else if (TryGetKubeConfigPath(out var kubeConfigPath))
             {
                 var config = K8sConfig.Load(kubeConfigPath);
-                if (IsGcpAuthProviderStrategy(config))
-                    authProviderStrategy = new GcpAuthProviderStrategy();
                 options = config.ToKubeClientOptions(defaultKubeNamespace: "default");
                 _logger.LogInformation($"Using kube config ({options.ApiEndPoint}).");
             }
@@ -43,16 +38,7 @@ namespace Contrib.KubeClient.CustomResources
                 _logger.LogInformation($"Using cluster-internal kubernetes connection ({options.ApiEndPoint}).");
             }
 
-            if (authProviderStrategy != null)
-                return KubeApiClient.Create(authProviderStrategy, options);
             return KubeApiClient.Create(options);
-        }
-
-        private bool IsGcpAuthProviderStrategy(K8sConfig config)
-        {
-            var targetContext = config.Contexts.Find(context => context.Name == config.CurrentContextName);
-            var userIdentity = config.UserIdentities.Find(user => user.Name == targetContext.Config.UserName);
-            return userIdentity.Config.AuthProvider.Name == "gcp";
         }
 
         private static bool TryGetKubeConfigPath(out string path)
