240ccd565b14edd67f32645f89eda3469246d2c7
Bastian Eicher <bastian.eicher@gft.com>
2019-12-11 12:11:31 +0100

    Made build script more robust

--- src/build.ps1
+++ src/build.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-dotnet clean
 dotnet msbuild /t:Restore /t:Build /p:Configuration=Release /p:Version=$Version
+if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 
 popd
